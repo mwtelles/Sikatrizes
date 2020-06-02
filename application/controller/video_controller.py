52129f848a08ce12aa3f38621479b341c5a64afd
Matheus Telles <mwtelles2012@hotmail.com>
2020-06-02 16:38:36 -0300

    adicionando a opção de likes(curtidas) dentro do projeto

--- application/controller/video_controller.py
+++ application/controller/video_controller.py
@@ -4,6 +4,17 @@ from application.model.dao.category_dao import categoryDao
 from application.model.dao.video_dao import videoDao
 from application.model.entity.comment import Comment
 
+@app.route("/video/like", methods = ["POST"])
+def like():
+    try:
+        videos = current_app.config ['videos']
+        video_id = (request.form["video_id"])
+        videosa = videos.get_video_by_id(video_id)
+        videosa.setLikes(videosa.getLikes()+1)
+        return jsonify(countLikes = videosa.getLikes())
+    except Exception as e:
+        return str(e)
+
 @app.route("/categories/<int:category_id>/videos/<int:video_id>")
 def video(category_id, video_id):
     videos = current_app.config ['videos']
@@ -14,4 +25,7 @@ def video(category_id, video_id):
     videory.setViews(videory.getViews()+1)
 
     return render_template('comment.html', videory = videory, category = category)
+
+
+
     
